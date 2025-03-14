2ecb2e239688e3f7ad94b68853c53110bfde78fc
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-03-15 06:45:07 +0800

    Platform info is written to NPM PKG and NAME to enable dynamic library fetching based on ENV
    Universal Building for 'general' and 'platform-specific' NPM PKGs

--- poodio/build.js
+++ poodio/build.js
@@ -1,50 +1,79 @@
 #! /usr/bin/env node
 
 // Import the modules
-globalThis.fs = require("fs");
-globalThis.path = require("path");
-globalThis.parseToml = require("smol-toml").parse;
-globalThis.parseArgs = require("minimist");
 globalThis.execSync = require("node:child_process").execSync;
+globalThis.fs = require("node:fs");
+globalThis.parseArgs = require("minimist");
+globalThis.parseToml = require("smol-toml").parse;
+globalThis.path = require("node:path");
 
 // Resolve the paths
 process.chdir(__dirname);
 const CPAR = require.resolve("cargo-cp-artifact/bin/cargo-cp-artifact");
 const NAPI = require.resolve("@napi-rs/cli/scripts");
-const DDIR = "dist/npm/";
-const FDIR = "src/node/";
+const TDIR = "dist/npm/";
+const SDIR = "src/node/";
 
 // Fetch the arguments
 const { features, target } = parseArgs(process.argv.slice(2));
-if (!target) {
-    throw new TypeError("Missing argument: --target <platform>");
-}
 const featuresArg = features ? `--features ${features} ` : "";
-const targetArg = `--target ${target} `;
+const targetArg = target ? `--target ${target} ` : "";
 const { name } = parseToml(fs.readFileSync("Cargo.toml", "utf8")).package;
 
 // Build the artifacts
 execSync(
     `\
-${CPAR} --artifact bin ${name} ${DDIR}${FDIR}/${name} \
--- ${NAPI} build ${targetArg}${featuresArg}--no-dts-header --release \
---cargo-flags='--locked --message-format json' ${DDIR}${FDIR}/`,
+${CPAR} --artifact bin ${name} ${TDIR}${SDIR}/${name} \
+-- ${NAPI} build ${featuresArg}${targetArg}${targetArg && "--release "}--no-dts-header \
+--cargo-flags='--locked --message-format json' ${TDIR}${SDIR}/`,
     { stdio: "inherit", windowsHide: true }
 );
 
-// NOTE: Assume that built artifacts are platform-specific
+if (target) {
+    // Rewrite the package info
+    const npmTarget = cargoToNpmTarget(target);
+    const npmTargetTriple = `${npmTarget.cpu[0]}-${npmTarget.os[0]}-${
+        npmTarget.libc[0] || "unknown"
+    }`;
+    const npmPkg = Object.assign(require("./package.json"), {
+        name: `@${name}/${name}-${npmTargetTriple}`,
+        optionalDependencies: undefined,
+        scripts: undefined,
+        ...npmTarget,
+    });
+    const npmPkgSorted = Object.fromEntries(Object.entries(npmPkg).sort());
+    fs.writeFileSync(`${TDIR}package.json`, JSON.stringify(npmPkgSorted, null, 2));
+} else {
+    // Rewrite the package info
+    const npmPkg = Object.assign(require("./package.json"), {
+        scripts: undefined,
+    });
+    fs.writeFileSync(`${TDIR}package.json`, JSON.stringify(npmPkg, null, 2));
 
-// Rewrite the package.json
-const npmPkg = Object.assign(require("./package.json"), {
-    name: `@${name}/${name}-${target}`,
-    optionalDependencies: undefined,
-    scripts: undefined,
-});
-fs.writeFileSync(`${DDIR}package.json`, JSON.stringify(npmPkg, null, 2));
+    // Overwrite the specific files
+    fs.cpSync(`${SDIR}/index.node`, `${TDIR}${SDIR}/index.node`);
+    fs.cpSync(`${SDIR}/poodio`, `${TDIR}${SDIR}/poodio`);
+}
 
-// Copy the README and LICENSE files
-fs.cpSync("README.md", `${DDIR}README.md`);
-fs.cpSync("LICENSE.txt", `${DDIR}LICENSE.txt`, { dereference: true });
+// Copy the universal files
+fs.cpSync("README.md", `${TDIR}README.md`);
+fs.cpSync("LICENSE.txt", `${TDIR}LICENSE.txt`, { dereference: true });
+fs.cpSync(`${SDIR}/index.js`, `${TDIR}${SDIR}/index.js`);
 
-// Copy the JavaScript entry point
-fs.cpSync(`${FDIR}/index.js`, `${DDIR}${FDIR}/index.js`);
+function cargoToNpmTarget(cargoTarget) {
+    const transform = {
+        "aarch64-apple-darwin": { os: ["darwin"], cpu: ["arm64"], libc: [] },
+        "aarch64-unknown-linux-gnu": { os: ["linux"], cpu: ["arm64"], libc: ["glibc"] },
+        "aarch64-pc-windows-msvc": { os: ["win32"], cpu: ["arm64"], libc: [] },
+        "i686-pc-windows-msvc": { os: ["win32"], cpu: ["ia32"], libc: [] },
+        "i686-unknown-linux-gnu": { os: ["linux"], cpu: ["ia32"], libc: ["glibc"] },
+        "x86_64-apple-darwin": { os: ["darwin"], cpu: ["x64"], libc: [] },
+        "x86_64-pc-windows-msvc": { os: ["win32"], cpu: ["x64"], libc: [] },
+        "x86_64-unknown-linux-gnu": { os: ["linux"], cpu: ["x64"], libc: ["glibc"] },
+    };
+    const npmTarget = transform[cargoTarget];
+    if (!npmTarget) {
+        throw new TypeError(`Unsupported target: "${cargoTarget}"`);
+    }
+    return npmTarget;
+}
