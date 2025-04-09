669d5206f1808075b9c535e73c76ded005df703c
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-09 20:55:16 +0800

    Add basic e2e tests

--- poodio/test/e2e/bin/test.cjs
+++ poodio/test/e2e/bin/test.cjs
@@ -6,16 +6,25 @@ const fs = require("node:fs");
 const { test } = require("uvu");
 const assert = require("uvu/assert");
 
-test("The executable has been built", () => {
-    const file =
-        "../../../dist/bin/poodio" + (process.platform === "win32" ? ".exe" : "");
-    assert.ok(fs.existsSync(file), `Not found: ${file} (cwd: ${process.cwd()})`);
-    const stat = fs.statSync(file);
-    assert.ok(stat.isFile(), `Not a file: ${file}`);
+const BIN_PATH =
+    "../../../dist/bin/poodio" + (process.platform === "win32" ? ".exe" : "");
+
+test("Executable is runnable", () => {
+    assert.ok(fs.existsSync(BIN_PATH), `Not found: ${BIN_PATH} (cwd: ${process.cwd()})`);
+    const stat = fs.statSync(BIN_PATH);
+    assert.ok(stat.isFile(), `Not a file: ${BIN_PATH}`);
     assert.ok(
         stat.mode & 0o111,
-        `Not an executable: ${file} (mode: ${stat.mode.toString(8)})`,
+        `Not an executable: ${BIN_PATH} (mode: ${stat.mode.toString(8)})`,
     );
+});
+
+test("Executable default output is correct", () => {
+    const output = require("node:child_process").execSync(BIN_PATH, {
+        encoding: "utf8",
+        windowsHide: true,
+    });
+    assert.is(output, "Greetings from poodio!\n");
 });
 
 test.run();
--- poodio/test/e2e/npm/test.cjs
+++ poodio/test/e2e/npm/test.cjs
@@ -1,1 +1,21 @@
 #! /usr/bin/env node
+
+process.chdir(__dirname);
+
+const { test } = require("uvu");
+const assert = require("uvu/assert");
+
+const PKG_PATH = "../../../dist/npm";
+
+test("Package modules can be required", () => {
+    assert.not.throws(() => require(PKG_PATH), "No default module");
+    assert.not.throws(() => require(PKG_PATH + "/package.json"), "No package metadata");
+    assert.not.throws(() => require(PKG_PATH + "/src/node/index.cjs"), "No main module");
+});
+
+test("Function 'greeting' default output is correct", () => {
+    const output = require(PKG_PATH).greeting();
+    assert.is(output, "Greetings from poodio!");
+});
+
+test.run();
