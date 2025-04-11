b7b3b5e4268c247e6949bdff484549649a97c909
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-11 20:35:00 +0800

    CI give up musl, try fix i686 windows CI

--- poodio/test/e2e/npm/test.cjs
+++ poodio/test/e2e/npm/test.cjs
@@ -2,26 +2,24 @@
 
 process.chdir(__dirname);
 
+const { resolve } = require("node:path");
 const { test } = require("uvu");
 const assert = require("uvu/assert");
 
-const PKG_PATH = "../../../dist/npm/";
+const PKG_PATH = resolve("../../../dist/npm");
 
 test("Package modules can be required or resolved", () => {
-    assert.not.throws(() => require(PKG_PATH));
-    assert.not.throws(() => require(`${PKG_PATH}src/node/index.cjs`));
-    assert.not.throws(() => require(`${PKG_PATH}package.json`));
+    assert.not.throws(() => require(PKG_PATH), `No module: '${PKG_PATH}'`);
+    assert.not.throws(() => require(`${PKG_PATH}/src/node/index.cjs`), "No main module");
+    assert.not.throws(() => require(`${PKG_PATH}/package.json`), "No package manifest");
 });
 
 test("Package executable version is correct", () => {
     const output = require("node:child_process")
         .execFileSync(
-            require.resolve(`${PKG_PATH}${require(`${PKG_PATH}package.json`).bin}`),
+            require.resolve(`${PKG_PATH}${require(`${PKG_PATH}/package.json`).bin}`),
             ["--version"],
-            {
-                encoding: "utf8",
-                windowsHide: true,
-            },
+            { encoding: "utf8", windowsHide: true },
         )
         .trimEnd();
     assert.is(output, answerVersion());
