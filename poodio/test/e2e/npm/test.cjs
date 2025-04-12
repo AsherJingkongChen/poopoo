c1881c71b7b29b7c697ffa92b02961375c3a0984
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-13 01:20:47 +0800

    CI test e2e-npm for incompat platform should throws

--- poodio/test/e2e/npm/test.cjs
+++ poodio/test/e2e/npm/test.cjs
@@ -4,23 +4,32 @@ process.chdir(__dirname);
 
 const { test } = require("uvu");
 const assert = require("uvu/assert");
+const { execSync } = require("node:child_process");
 
 test.before(() => {
-    require("node:child_process").execSync("npm i --no-save ../../../dist/npm/", {
-        stdio: "inherit",
-        windowsHide: true,
-    });
+    assert.not.throws(() =>
+        execSync("npm i --no-save ../../../dist/npm/", {
+            stdio: "inherit",
+            windowsHide: true,
+        }),
+    );
 });
 
-test("Package can be required", () => require("poodio"));
-
-test("Package executable version is correct", () => {
-    const output = require("node:child_process")
-        .execSync("npx -y poodio --version", {
+test("Package can be required", () => {
+    assert.not.throws(
+        () => require("poodio"),
+        execSync("npm ls", {
             encoding: "utf8",
             windowsHide: true,
-        })
-        .slice(0, -1);
+        }),
+    );
+});
+
+test("Package executable version is correct", () => {
+    const output = execSync("npx -y poodio --version", {
+        encoding: "utf8",
+        windowsHide: true,
+    }).slice(0, -1);
     assert.is(output, answerVersion());
 });
 
