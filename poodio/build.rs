6e7b93c45ce37d6e16481433ecb064b0f02dc457
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-14 19:52:02 +0800

    Rename npm package directories

--- poodio/build.rs
+++ poodio/build.rs
@@ -57,19 +57,19 @@ fn build_npm_dist() -> Result<()> {
         .wrap_err("Unsupported target")?;
 
     let dist_dir = Path::new("dist/npm");
-    let dist_plat_dir = dist_dir.join("plat");
-    let dist_plat_cfg_file = dist_plat_dir.join("package.json");
-    let dist_plat_license_file = dist_plat_dir.join("LICENSE.txt");
-    let dist_plat_readme_file = dist_plat_dir.join("README.md");
-    let dist_pure_dir = dist_dir.join("pure");
-    let dist_pure_cfg_file = dist_pure_dir.join("package.json");
-    let dist_pure_license_file = dist_pure_dir.join("LICENSE.txt");
-    let dist_pure_readme_file = dist_pure_dir.join("README.md");
+    let dist_bind_dir = dist_dir.join("bind");
+    let dist_bind_cfg_file = dist_bind_dir.join("package.json");
+    let dist_bind_license_file = dist_bind_dir.join("LICENSE.txt");
+    let dist_bind_readme_file = dist_bind_dir.join("README.md");
+    let dist_wrap_dir = dist_dir.join("wrap");
+    let dist_wrap_cfg_file = dist_wrap_dir.join("package.json");
+    let dist_wrap_license_file = dist_wrap_dir.join("LICENSE.txt");
+    let dist_wrap_readme_file = dist_wrap_dir.join("README.md");
     let orig_license_file = Path::new("LICENSE.txt");
     let orig_readme_file = Path::new("README.md");
 
-    println!("cargo:rerun-if-changed={dist_plat_cfg_file:?}");
-    println!("cargo:rerun-if-changed={dist_pure_cfg_file:?}");
+    println!("cargo:rerun-if-changed={dist_bind_cfg_file:?}");
+    println!("cargo:rerun-if-changed={dist_wrap_cfg_file:?}");
 
     let cfg = json!({
         "author": env!("CARGO_PKG_AUTHORS"),
@@ -86,15 +86,15 @@ fn build_npm_dist() -> Result<()> {
         "version": version,
     });
 
-    let mut dist_plat_cfg = cfg.to_owned();
-    let o = dist_plat_cfg.as_object_mut().unwrap();
+    let mut dist_bind_cfg = cfg.to_owned();
+    let o = dist_bind_cfg.as_object_mut().unwrap();
     o.insert(
         "name".into(),
         format!("@{name}/{name}-{cpu}-{os}-{libc}").into(),
     );
 
-    let mut dist_pure_cfg = cfg.to_owned();
-    let o = dist_pure_cfg.as_object_mut().unwrap();
+    let mut dist_wrap_cfg = cfg.to_owned();
+    let o = dist_wrap_cfg.as_object_mut().unwrap();
     o.insert("bin".into(), json!({ name: "index.js" }));
     o.insert("dependencies".into(), json!({ "tell-libc": "^0.0.0" }));
     o.insert(
@@ -112,20 +112,20 @@ fn build_npm_dist() -> Result<()> {
     o.insert("name".into(), name.into());
 
     fs::remove_dir_all(dist_dir).ok();
-    fs::create_dir_all(&dist_plat_dir)?;
-    fs::create_dir_all(&dist_pure_dir)?;
+    fs::create_dir_all(&dist_bind_dir)?;
+    fs::create_dir_all(&dist_wrap_dir)?;
 
-    fs::copy(orig_license_file, &dist_plat_license_file)?;
-    fs::copy(orig_license_file, &dist_pure_license_file)?;
-    fs::copy(orig_readme_file, &dist_plat_readme_file)?;
-    fs::copy(orig_readme_file, &dist_pure_readme_file)?;
+    fs::copy(orig_license_file, &dist_bind_license_file)?;
+    fs::copy(orig_license_file, &dist_wrap_license_file)?;
+    fs::copy(orig_readme_file, &dist_bind_readme_file)?;
+    fs::copy(orig_readme_file, &dist_wrap_readme_file)?;
 
-    dist_plat_cfg.serialize(&mut Serializer::with_formatter(
-        &mut fs::File::create(&dist_plat_cfg_file)?,
+    dist_bind_cfg.serialize(&mut Serializer::with_formatter(
+        &mut fs::File::create(&dist_bind_cfg_file)?,
         PrettyFormatter::with_indent(b"    "),
     ))?;
-    dist_pure_cfg.serialize(&mut Serializer::with_formatter(
-        &mut fs::File::create(&dist_pure_cfg_file)?,
+    dist_wrap_cfg.serialize(&mut Serializer::with_formatter(
+        &mut fs::File::create(&dist_wrap_cfg_file)?,
         PrettyFormatter::with_indent(b"    "),
     ))?;
 
