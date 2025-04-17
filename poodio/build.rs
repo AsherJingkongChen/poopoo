a34bb906a45591d9c886c1b35e5d7f4ad7179657
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-17 23:50:27 +0800

    Adjust build scripts and rename artifact dir, Remove built cfgs

--- poodio/build.rs
+++ poodio/build.rs
@@ -25,35 +25,31 @@ mod bind_napi {
     use serde_json::{json, ser::PrettyFormatter, Serializer};
     use std::{collections::BTreeMap, fs, io::Write, path::Path, sync::LazyLock};
 
-    static CARGO_TO_NPM_TARGET: LazyLock<BTreeMap<String, [String; 3]>> = LazyLock::new(|| {
+    static CARGO_TO_NPM_TARGET: LazyLock<BTreeMap<&str, [&str; 3]>> = LazyLock::new(|| {
         [
             ("aarch64-apple-darwin", ["arm64", "darwin", "unknown"]),
             ("aarch64-pc-windows-msvc", ["arm64", "win32", "unknown"]),
             ("aarch64-unknown-linux-gnu", ["arm64", "linux", "glibc"]),
-            ("i686-pc-windows-msvc", ["ia32", "win32", "unknown"]),
-            ("i686-unknown-linux-gnu", ["ia32", "linux", "glibc"]),
             ("x86_64-apple-darwin", ["x64", "darwin", "unknown"]),
             ("x86_64-pc-windows-msvc", ["x64", "win32", "unknown"]),
             ("x86_64-unknown-linux-gnu", ["x64", "linux", "glibc"]),
         ]
         .into_iter()
-        .map(|(k, v)| (k.into(), v.map(Into::into)))
         .collect()
     });
 
-    static NPM_KEYWORDS: LazyLock<Vec<String>> = LazyLock::new(|| {
-        let mut result = r#"
-        addon agent ai api audio binding cli easy
-        fast fun funny joke lightweight llm mcp minimal
-        multimedia napi native node performance poodio
-        python rust server simple simulation sound speed test
-    "#
+    static NPM_KEYWORDS: LazyLock<Vec<&str>> = LazyLock::new(|| {
+        let mut v = r#"
+            addon agent ai api audio binding cli easy
+            fast fun funny joke lightweight llm mcp minimal
+            multimedia napi native node performance poodio
+            python rust server simple simulation sound speed test
+        "#
         .split_ascii_whitespace()
-        .map(Into::into)
         .collect::<Vec<_>>();
-        result.sort_unstable();
-        result.dedup();
-        result
+        v.sort_unstable();
+        v.dedup();
+        v
     });
 
     pub fn write_cfgs() -> Result<()> {
@@ -66,25 +62,20 @@ mod bind_napi {
         }
 
         let name = env!("CARGO_PKG_NAME");
-        let version = env!("CARGO_PKG_VERSION");
         let target = std::env::var("TARGET")?;
-        let [cpu, os, libc] = CARGO_TO_NPM_TARGET
-            .get(&target)
-            .cloned()
+        let version = env!("CARGO_PKG_VERSION");
+        let &[cpu, os, libc] = CARGO_TO_NPM_TARGET
+            .get(target.as_str())
             .wrap_err("Unsupported target")?;
 
-        let dist_dir = Path::new("dist/npm/");
-        let dist_bind_dir = dist_dir.join("bind/");
-        let dist_bind_cfg_file = dist_bind_dir.join("package.json");
-        let dist_bind_license_file = dist_bind_dir.join("LICENSE.txt");
-        let dist_bind_readme_file = dist_bind_dir.join("README.md");
-        let dist_wrap_dir = dist_dir.join("wrap/");
-        let dist_wrap_cfg_file = dist_wrap_dir.join("package.json");
-        let dist_wrap_license_file = dist_wrap_dir.join("LICENSE.txt");
-        let dist_wrap_readme_file = dist_wrap_dir.join("README.md");
+        let dir = Path::new("dist/npm/");
+        let common_dir = dir.join("common/");
+        let native_dir = dir.join("native/");
+        let config_name = Path::new("package.json");
+        let license_name = Path::new("LICENSE.txt");
+        let readme_name = Path::new(env!("CARGO_PKG_README"));
 
-        // cfg
-        let dist_cfg = json!({
+        let config = json!({
             "author": env!("CARGO_PKG_AUTHORS"),
             "description": env!("CARGO_PKG_DESCRIPTION"),
             "homepage": env!("CARGO_PKG_HOMEPAGE"),
@@ -98,49 +89,46 @@ mod bind_napi {
             "type": "commonjs",
             "version": version,
         });
-        let orig_license_file = Path::new("LICENSE.txt");
-        let orig_readme_file = Path::new("README.md");
 
-        // cfg
-        let mut dist_bind_cfg = dist_cfg.to_owned();
-        let o = dist_bind_cfg.as_object_mut().unwrap();
-        o.insert("cpu".into(), json!([cpu]));
+        let mut native_config = config.to_owned();
+        let v = native_config.as_object_mut().unwrap();
+        v.insert("cpu".into(), json!([cpu]));
         if libc != "unknown" {
-            o.insert("libc".into(), json!([libc]));
+            v.insert("libc".into(), json!([libc]));
         }
-        o.insert(
+        v.insert(
             "name".into(),
             format!("@{name}/{name}-{cpu}-{os}-{libc}").into(),
         );
-        o.insert("os".into(), json!([os]));
+        v.insert("os".into(), json!([os]));
 
-        // cfg
-        let mut dist_wrap_cfg = dist_cfg.to_owned();
-        let o = dist_wrap_cfg.as_object_mut().unwrap();
-        o.insert("bin".into(), json!({ name: "index.js" }));
-        o.insert("dependencies".into(), json!({ "tell-libc": "^0.0.0" }));
-        o.insert(
+        let mut common_config = config.to_owned();
+        let v = common_config.as_object_mut().unwrap();
+        v.insert("bin".into(), json!({ name: "index.js" }));
+        v.insert("dependencies".into(), json!({ "tell-libc": "^0.0.0" }));
+        v.insert(
             "optionalDependencies".into(),
             CARGO_TO_NPM_TARGET
                 .values()
-                .map(|[cpu, os, libc]| {
-                    (
-                        format!("@{name}/{name}-{cpu}-{os}-{libc}"),
-                        version.to_owned(),
-                    )
-                })
+                .map(|[cpu, os, libc]| (format!("@{name}/{name}-{cpu}-{os}-{libc}"), version))
                 .collect(),
         );
-        o.insert("name".into(), name.into());
+        v.insert("name".into(), name.into());
 
-        fs::create_dir_all(&dist_bind_dir)?;
-        fs::create_dir_all(&dist_wrap_dir)?;
-        fs::copy(orig_license_file, &dist_bind_license_file)?;
-        fs::copy(orig_license_file, &dist_wrap_license_file)?;
-        fs::copy(orig_readme_file, &dist_bind_readme_file)?;
-        fs::copy(orig_readme_file, &dist_wrap_readme_file)?;
-        write_json_pretty(fs::File::create(&dist_bind_cfg_file)?, &dist_bind_cfg)?;
-        write_json_pretty(fs::File::create(&dist_wrap_cfg_file)?, &dist_wrap_cfg)?;
+        fs::create_dir_all(&common_dir)?;
+        fs::create_dir_all(&native_dir)?;
+        fs::copy(license_name, common_dir.join(license_name))?;
+        fs::copy(license_name, native_dir.join(license_name))?;
+        fs::copy(readme_name, common_dir.join(readme_name))?;
+        fs::copy(readme_name, native_dir.join(readme_name))?;
+        write_json_pretty(
+            fs::File::create(common_dir.join(config_name))?,
+            &common_config,
+        )?;
+        write_json_pretty(
+            fs::File::create(native_dir.join(config_name))?,
+            &native_config,
+        )?;
 
         Ok(())
     }
