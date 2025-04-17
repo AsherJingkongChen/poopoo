02658cd1a712798b3114d37ac4b144762a7b1705
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-17 08:02:26 +0800

    CI fx uv cache

--- show-toolchains.sh
+++ show-toolchains.sh
@@ -1,3 +1,3 @@
 echo "rustup: $(rustup show active-toolchain)"
 echo "node: $(node --print 'let p=process;`${p.arch}-${p.platform}-${p.version}`')"
-echo "python (uv): $(uv python find --verbose 2>&1 | grep 'Found \`.*\` at' | awk -F'\`' '{print $2}')"
+echo "python (uv): $(uv python find --verbose 2>&1 | tail -n 2 | grep 'Found \`.*\` at' | awk -F'\`' '{print $2}')"
