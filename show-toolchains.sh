813ec46ae3605a7bfedbe1ae4a703815c7a1133e
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-04-17 08:59:39 +0800

    CI debug

--- show-toolchains.sh
+++ show-toolchains.sh
@@ -1,3 +1,4 @@
 echo "rustup: $(rustup show active-toolchain)"
 echo "node: $(node --print 'let p=process;`${p.arch}-${p.platform}-${p.version}`')"
 echo "python (uv): $(uv python find --verbose 2>&1 | tail -n 2 | perl -ne 'print $1 if /`([^`]+)`/')"
+uv python find --verbose 2>&1 # Debug
