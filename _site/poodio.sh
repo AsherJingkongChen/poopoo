ade51402e2c2c68e0303e51cb7036199935a5c97
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2025-05-04 05:27:40 +0800

    Correct log condition

--- _site/poodio.sh
+++ _site/poodio.sh
@@ -26,8 +26,8 @@ log 'NAME:' "${NAME}"
 log 'REPO:' "${REPO}"
 
 # Fetching the release tag
-log 'TAG:' 'Fetching ...'
 TAG="${NAME}@${1:-"$(
+  log 'TAG:' 'Fetching ...'
   curl -LSsf \
     -H 'Accept: application/vnd.github+json' \
     -H 'X-GitHub-Api-Version: 2022-11-28' \
